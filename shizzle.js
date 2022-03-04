ba1c72dff34ae50173f3afd360a75f950c878981
Sumit Shubham <sumitshubhamr7@gmail.com>
2022-03-04 22:17:20 +0530

    initial commit

---
+++ shizzle.js
@@ -0,0 +1,29 @@
+function incrementQ(id){
+    var data = parseInt(document.getElementById(id).innerHTML);
+    data = data + 1;
+    document.getElementById(id).innerHTML= data;
+    calculateTotalPrice("price"+id,"Add");
+}
+
+function decrementQ(id){
+    var data = parseInt(document.getElementById(id).innerHTML);
+    data = data - 1;
+    if(data<0)
+        document.getElementById(id).innerHTML= 0;
+    else{
+        document.getElementById(id).innerHTML= data;
+        calculateTotalPrice("price"+id,"Sub");
+    }
+}
+
+function calculateTotalPrice(priceId,cmd){
+    var price = parseInt(document.getElementById(priceId).innerHTML);
+    var id = "calcPrice";
+    var total = parseInt(document.getElementById(id).innerHTML);
+    if(cmd=="Add"){
+        document.getElementById(id).innerHTML = (total+price);
+    }
+    else{
+        document.getElementById(id).innerHTML = (total-price);
+    }
+}
